c775e829362e72810556cb2b929c3ca8f05ce0dc
JordiMartinezDev <jordiupc87@gmail.com>
2024-05-23 10:29:25 +0200

    Human class

---
+++ src/newPackageTest/Earth.java
@@ -0,0 +1,14 @@
+package newPackageTest;
+
+public class Earth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Human tom = new Human();
+		
+		tom.speak();
+	}
+	
+
+}

2c5ad72ca821cdacf149a49ee8c81ffe3cc37b4d
JordiMartinezDev <jordiupc87@gmail.com>
2024-05-23 10:15:28 +0200

    Testing methods static

---
+++ src/newPackageTest/AnotherPackage.java
@@ -0,0 +1,8 @@
+package newPackageTest;
+
+public class AnotherPackage {
+
+	public static void staticMethod() {
+		System.out.println("From second package, static method");
+	}
+}
---
+++ src/testGit/TestNewClass.java
@@ -0,0 +1,7 @@
+package testGit;
+
+public class TestNewClass {
+	public static void staticMethodSamePackage() {
+		System.out.println("From same package,different class, static method");
+	}
+}

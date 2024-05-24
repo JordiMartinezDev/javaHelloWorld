8081faa9bf51a46821f458336078f790ad4758de
JordiMartinezDev <jordimartinezdev@gmail.com>
2024-05-24 06:16:06 +0000

    Testing cloud git

--- src/testGit/TestNewClass.java
+++ src/testGit/TestNewClass.java
@@ -3,5 +3,6 @@ package testGit;
 public class TestNewClass {
 	public static void staticMethodSamePackage() {
 		System.out.println("From same package,different class, static method");
+		System.out.println("testing cloud");
 	}
 }
